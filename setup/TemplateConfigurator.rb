5d3c55a22ed0d4b2e832bbde557e946887afc5d7
夏常聆 <13718037163@163.com>
2021-08-29 20:43:20 +0800

    pod template

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -30,6 +30,7 @@ module Pod
       clean_template_files
       customise_prefix
       rename_spec_files
+      reinitialize_git_repo
       run_pod_install
 
       @message_bank.farewell_message
@@ -82,6 +83,13 @@ module Pod
     #spec文件重命名
     def rename_spec_files
       File.rename("NAME.podspec", @pod_name + ".podspec")
+    end
+
+    #重新Git初始化
+    def reinitialize_git_repo
+      `rm -rf .git`
+      `git init`
+      `git add -A`
     end
 
     #验证用户信息
